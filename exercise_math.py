3cd3ca354ac5002922b833f78ef8441a2ddaeff3
ianconnonUA <ivconnongarzia@mail.austral.edu.ar>
2025-03-17 11:32:30 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,10 @@
 def math():
     a = 57
     b = 7
+    print(a+b) #Muestra la suma del entero a y el entero b
+    print(a-b) #Muestra la resta del " " " " " "
+    print(a*b) #Muestra el producto del " " " " " "
+    print((a+b)/2) #Muestra el promedio del " " " " " "
+    print(round(a/b)) #Muestra el cociente de la division de ambos valores (redondea el valor, para convertirlo en entero)
+    print(round(a%b)) #Muestra el resto de la division entera
+    print(a/b) #Muestra la division de ambos valores
